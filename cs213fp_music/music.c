64d401c73c215bf3cc7954ee1520db1068823702
Sam <rudenber@grinnell.edu>
2023-11-18 17:17:53 -0600

    music files

---
+++ cs213fp_music/music.c
@@ -0,0 +1,65 @@
+#include <SDL2/SDL.h>
+#include <math.h>
+
+#define SAMPLE_RATE 44100
+#define AMPLITUDE 32767
+#define DURATION 1.0 // in seconds
+
+void generateSineWave(Uint8 *buffer, double frequency) {
+    Uint32 length = SAMPLE_RATE * DURATION;
+    double timeStep = 1.0 / SAMPLE_RATE;
+    
+    for (Uint32 i = 0; i < length; ++i) {
+        double t = i * timeStep;
+        double wave = sin(2.0 * M_PI * frequency * t);
+        Sint16 sample = (Sint16)(wave * AMPLITUDE);
+        
+        buffer[i * 2] = (Uint8)(sample & 0xFF);
+        buffer[i * 2 + 1] = (Uint8)((sample >> 8) & 0xFF);
+    }
+}
+
+int main() {
+    if (SDL_Init(SDL_INIT_AUDIO) < 0) {
+        fprintf(stderr, "SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    SDL_AudioSpec audioSpec;
+    audioSpec.freq = SAMPLE_RATE;
+    audioSpec.format = AUDIO_S16SYS;
+    audioSpec.channels = 1;  // 1 for mono, 2 for stereo
+    audioSpec.samples = 4096;
+    audioSpec.callback = NULL;
+
+    SDL_AudioDeviceID audioDevice = SDL_OpenAudioDevice(NULL, 0, &audioSpec, NULL, 0);
+    if (audioDevice == 0) {
+        fprintf(stderr, "SDL could not open audio device! SDL_Error: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    SDL_PauseAudioDevice(audioDevice, 0);
+
+    // List of frequencies to play
+    double frequencies[] = {261.63, 329.63, 392.00, 523.25};  // C4, E4, G4, C5
+
+    for (int i = 0; i < sizeof(frequencies) / sizeof(frequencies[0]); ++i) {
+        Uint32 length = SAMPLE_RATE * DURATION;
+        Uint8 *buffer = (Uint8 *)malloc(length * 2);  // 2 bytes per sample for AUDIO_S16SYS
+
+        generateSineWave(buffer, frequencies[i]);
+        SDL_QueueAudio(audioDevice, buffer, length * 2);
+
+        free(buffer);
+
+        // Wait for the sound to finish
+        SDL_Delay(DURATION * 1000);
+    }
+
+    SDL_CloseAudioDevice(audioDevice);
+    SDL_Quit();
+
+    return 0;
+}
+
+
